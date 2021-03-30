a6a7eacc265c9fe39fd3d9a1dff55f1361b67472
jarbson costa <jarbsonfc@gmail.com>
2021-03-30 12:52:38 -0300

    refatorando o model de category

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,9 +1,18 @@
 import { uuid } from 'uuidv4';
+import {Column, CreateDateColumn, Entity, PrimaryColumn} from 'typeorm'
+
+
+@Entity("categories")
 
 class Category {
+    @PrimaryColumn()
     id?: string;
+    @Column()
     name: string;
+    @Column()
     description: string;
+
+    @CreateDateColumn()
     created_at: Date;
 
     constructor() {
