733d7c529ead31872e44860af62240dc9ab30b87
jarbson costa <jarbsonfc@gmail.com>
2021-04-05 10:02:15 -0300

    criando migration e entities de User

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,33 @@
+import {v4 as uuidv4 } from 'uuid'
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
+
+
+@Entity("users")
+default class User{
+    @PrimaryColumn()
+    id?: string;
+    @Column()
+    name: string;
+    @Column()
+    username: string;
+    @Column()
+    password: string;
+    @Column()
+    email: string;
+    @Column()
+    driver_license: string;
+    @Column()
+    isAdmin: boolean;
+    @CreateDateColumn()
+    created_at: Date
+    
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuidv4()
+            
+        }
+    }
+}
+
+export default User
