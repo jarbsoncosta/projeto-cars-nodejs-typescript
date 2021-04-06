d036499bfac7cfbd1545ede9e92e47ea3a9014aa
jarbson costa <jarbsonfc@gmail.com>
2021-04-06 09:38:36 -0300

    criptografando password do user

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,5 @@
-import {inject, injectable} from 'tsyringe'
+import { inject, injectable } from 'tsyringe'
+import {hash} from 'bcryptjs'
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import {ICreateUserDTO} from '../../dtos/ICreateUserDTO'
 
@@ -14,9 +15,18 @@ class CreateUserUseCase{
         email,
         driver_license
     }: ICreateUserDTO): Promise<void>{
+
+        const userAlreadyExists = await this.usersRepository.findByEmail(email);
+        if (userAlreadyExists) {
+            throw new Error("User Already Exists");
+            
+        }
+        
+
+        const passwordHash = await hash(password, 8);
         await this.usersRepository.create({
             name,
-            password,
+            password: passwordHash,
             email,
             driver_license,
         
