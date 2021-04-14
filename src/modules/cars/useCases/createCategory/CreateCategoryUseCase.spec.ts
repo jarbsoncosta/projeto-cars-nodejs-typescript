9590c0eb676363e3006a5864d57ea80b19b23fd9
jarbson costa <jarbsonfc@gmail.com>
2021-04-14 11:44:40 -0300

    Implementando teste de criação de categoria

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,50 @@
+import AppError from '../../../../errors/AppError';
+import { CategoriesRepositoryInMemory } from '../../repositories/in-memory/CategoriesRepositoryInMemory';
+import CreateCategoryUseCase from './CreateCategoryUseCase';
+
+let createCategoryUseCase: CreateCategoryUseCase;
+let createRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe('Create Category', () => {
+    beforeEach(() => {
+        createRepositoryInMemory = new CategoriesRepositoryInMemory();
+        createCategoryUseCase = new CreateCategoryUseCase(
+            createRepositoryInMemory,
+        );
+    });
+
+    // deve ser capaz de criar uma nova categoria
+    it('should be able to create a new category', async () => {
+        const category = {
+            name: 'Category Test',
+            description: 'Category description Test',
+        };
+
+        await createCategoryUseCase.execute({
+            name: category.name,
+            description: category.description,
+        });
+        const categoryCreated = await createRepositoryInMemory.findByName(
+            category.name,
+        );
+        expect(categoryCreated).toHaveProperty('id');
+    });
+    // não deve ser capaz de criar uma nova categoria com o nome existente
+    it('shoutd not be able to create a new category with name exists', async () => {
+        expect(async () => {
+            const category = {
+                name: 'Category Test',
+                description: 'Category description Test',
+            };
+
+            await createCategoryUseCase.execute({
+                name: category.name,
+                description: category.description,
+            });
+            await createCategoryUseCase.execute({
+                name: category.name,
+                description: category.description,
+            });
+        }).rejects.toBeInstanceOf(AppError);
+    });
+});
