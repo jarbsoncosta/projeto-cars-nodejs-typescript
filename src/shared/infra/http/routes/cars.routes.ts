865758e30496c8a5b2c1ac260bda869f58a09afa
jarbson costa <jarbsonfc@gmail.com>
2021-05-09 00:02:28 -0300

    criando middleware de admin

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -2,12 +2,18 @@ import { Router } from 'express';
 
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
 
+import { ensureAdmin } from '../middlewares/ensureAdmin';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const carsRouters = Router();
 
 const createCarController = new CreateCarController();
 
-carsRouters.post('/', ensureAuthenticated, createCarController.handle);
+carsRouters.post(
+    '/',
+    ensureAuthenticated,
+    ensureAdmin,
+    createCarController.handle,
+);
 
 export default carsRouters;
