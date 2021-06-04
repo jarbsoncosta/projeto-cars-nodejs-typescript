3e624a39a24cd0fc8c036c82b32d500a91d0e8fb
jarbson costa <jarbsonfc@gmail.com>
2021-06-03 23:22:05 -0300

    migration de createCarImages

---
+++ src/shared/infra/typeorm/migrations/1622772439728-creatCarImages.ts
@@ -0,0 +1,41 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class creatCarImages1622772439728 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'car_image',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                    },
+                    {
+                        name: 'image_name',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                ],
+                foreignKeys: [
+                    {
+                        name: 'FKCarImage',
+                        referencedTableName: 'cars',
+                        referencedColumnNames: ['id'],
+                        columnNames: ['car_id'],
+                        onDelete: 'SET NULL',
+                        onUpdate: 'SET NULL',
+                    },
+                ],
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('car_image');
+    }
+}
